fb5fec2751e7373a15fe3ed946990d584a696645
aa <aa>
2022-01-02 23:04:58 +0800

    有报错

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import React, { useState,useEffect,useReducer } from 'react';
+import React, { useState,useEffect,useReducer,useMemo,useCallback ,memo} from 'react';
 
 function App() {
-  const [count, dispathCount] = useReducer( (state,dispath)=>{
-    
-  } ,0 )
+  const [count, dispathCount] = useReducer( (state,action)=>{
+    switch (action.type) {
+      case 'add':return count+1;
+      case 'minus':return count-1;
+      default: return state
+    }
+  } , 0 )
   const [name, setName] = useState('jokcy')
+
+  const config = useMemo(() => {
+    return {
+      text:`count is ${count}`,
+      color:count>3?'red':'blue'
+    }
+  }, [count])
 
   const handleButtonClick = useCallback(
     () => {
-      dispathCount
+      dispathCount({type:'add'})
     },
     [],
   )
 
   return (
     <div className="App">
-      <input type="text" value={name} onChange={} />
+      <input type="text" value={name} onChange={(e)=>{setName(e.target.value)}} />
+      <Child config={config} onButtonClick={handleButtonClick} />
     </div>
   );
 }
+
+const Child = memo(function Child({onButtonClick,config}){
+  console.log('child render');
+  return (
+    <button onClick={onButtonClick} style={ {color:config.color} } >
+      {config.text}
+    </button>
+  )
+})
 
 export default App;
