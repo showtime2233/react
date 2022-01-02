286a9f2c07df8a30bcffefcd98053712f49a93cc
aa <aa>
2022-01-02 18:40:38 +0800

    删除useReducer

--- src/App.js
+++ src/App.js
@@ -3,20 +3,11 @@ import React, { useState,useEffect,useReducer } from 'react';
 function App() {
   const [count, setcount] = useState(0)
 
-  const countReducer = (state,action)=>{
-    switch (action.type) {
-      case 'add':return state+1;
-      case 'minus':return state-1;
-      default:return state
-    }
-  }
-
-  const [num, numDispatch] = useReducer(countReducer, 0)
   
   useEffect(() => {
     const interval = setInterval( ()=>{
-      // setcount(count+1)
-      setcount(count=>count+1)
+      setcount(count+1)
+      // setcount(count=>count+1)
     },1000 )
     return () => {
       
@@ -24,12 +15,11 @@ function App() {
   }, [])
 
   let add = ()=>{
-    numDispatch({type:'add'})
+    setcount(10)
   }
 
   return (
     <div className="App">
-      {num}
       <div>{count}</div>
       <button onClick={add}>add</button>
     </div>
